1e3fbdfb443aba17a2cc9abe95bc2f53909ba5ff
代码别生气 <cyssxt@163.com>
2022-04-24 22:03:27 +0800

    机器人推送

--- src/main/java/com/cyssxt/telegrambotsdk4j/BotRegister.java
+++ src/main/java/com/cyssxt/telegrambotsdk4j/BotRegister.java
@@ -3,6 +3,7 @@ package com.cyssxt.telegrambotsdk4j;
 import com.cyssxt.telegrambotsdk4j.methods.ActionFactory;
 import lombok.extern.slf4j.Slf4j;
 
+import java.time.LocalDateTime;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.ExecutorService;
@@ -30,6 +31,7 @@ public class BotRegister {
             executorService.submit(() -> {
                 while (true){
                     WaitSend poll;
+                    log.info("init={}", LocalDateTime.now());
                     while ((poll= WAIT_SEND.poll())!=null){
                         log.info("init={}",poll);
                         ActionFactory.quickSend(poll.getKey(),poll.getMessage());
